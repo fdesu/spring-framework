9abc3a34880c3105bde50f0ce1ba7868486ea93a
Sergei Ustimenko <merkel05@gmail.com>
2016-11-30 23:56:16 +0100

    Delete unnecessary reinjecting on original context

--- spring-test/src/main/java/org/springframework/test/context/TestContextManager.java
+++ spring-test/src/main/java/org/springframework/test/context/TestContextManager.java
@@ -303,7 +303,6 @@ public class TestContextManager {
             UntypedAnnotationDescriptor descriptor = findAnnotationDescriptorForTypes(testMethod,
                     ContextConfiguration.class, ContextHierarchy.class);
             if (descriptor != null) {
-                originalTestContext.setAttribute(REINJECT_DEPENDENCIES_ATTRIBUTE, Boolean.TRUE);
                 return createThrowAwayContext(testMethod, originalTestContext);
             }
         }
